707cdf3dd85bb336c574dbb1e08ec7b76204d954
Tomas <liberfran.04@gmail.com>
2025-08-02 23:21:08 -0300

    Correcciones en la intro principalmente. Marcaba muchos errores con los 'useRef()'.

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,48 +1,57 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useState } from "react";
 
 function Intro() {
-    const logoRef = useRef(null);
-    const loaderRef = useRef(null);
-    const containerRef = useRef(null);
-
-
-    useEffect(() => {
-        if (logoRef.current) {
-            setTimeout(() => {
-                logoRef.current.style.opacity = 1
-            }, 500);
-        }
-    }, []);
-
-    document.body.classList.add('overflow-hidden')
+    const [logoClass, setLogoClass] = useState({
+        opacity: 0,
+        scale: 1,
+    });
+    const [loaderClass, setLoaderClass] = useState({
+        opacity: 0,
+    });
+    const [containerBackground, setContainerBackground] = useState('bg-black'); // inicialmente la pantalla será negra
 
     useEffect(() => {
         setTimeout(() => {
-            if (loaderRef.current && containerRef.current) { // Primer efecto a los 4s
-                loaderRef.current.style.opacity = 1;
-                setTimeout(() => { // siguiente efecto a los 6s
-                    loaderRef.current.style.opacity = 0;
-                    setTimeout(() => { // tercer efecto a los 7s
-                        logoRef.current.style.scale = 25;
-                        logoRef.current.style.opacity = 0;
-                        containerRef.current.classList.replace('bg-black', 'bg-transparent');
-                        setTimeout(() => { // ultimo efecto a los 10s (desaparece la imagen)
-                            containerRef.current.remove();
-                            document.body.classList.remove('overflow-hidden');
-                        }, 2300);
-                    }, 1000);
-                }, 2000)
-            }
+            setLogoClass({
+                opacity: 1,
+                scale: 1,
+            });
+        }, 500);
+    }, []);
+
+    useEffect(() => {
+        setTimeout(() => { //primer efecto a los 3s
+            setLoaderClass({
+                opacity: 1
+            })
+            setTimeout(() => { // siguiente efecto a los 5s
+                setLoaderClass({
+                    opacity: 0
+                })
+                setTimeout(() => { // tercer efecto a los 6s
+                    setLogoClass({
+                        opacity: 0,
+                        scale: 25,
+                    });
+
+                    setContainerBackground('bg-transparent');
+
+                    setTimeout(() => { // ultimo efecto a los 9.3s (final)
+                        setContainerBackground('hidden');
+                        document.body.classList.remove('overflow-hidden');
+                    }, 2300);
+                }, 1000);
+            }, 2000)
         }, 3000);
     }, [])
-    return <div ref={containerRef} className='fixed z-5 h-screen w-screen flex flex-col gap-8 items-center justify-center bg-black text-gray-200 transition-background duration-2300 ease'>
+    return <div className={'fixed z-5 h-screen w-screen flex flex-col gap-8 items-center justify-center text-gray-200 transition-background duration-2300 ease ' + containerBackground}>
         <img
-            ref={logoRef} // apunto a este elemento
             src="/src/assets/logo-fma.png"
             alt="Logo FMA"
             className='min-w-[100px] max-w-3xs w-full transition-scale ease duration-2300 opacity-0 mx-2'
+            style={logoClass}
         />
-        <span className="loader opacity-0 transition-opacity duration-1000 ease animate-spin" ref={loaderRef}></span>
+        <span className="loader opacity-0 transition-opacity duration-1000 ease animate-spin" style={loaderClass}></span>
     </div>;
 }
 
