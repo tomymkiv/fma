3e6bb33cbe2aadba43e66f509077c7824062fad2
Tomas <liberfran.04@gmail.com>
2025-08-09 17:22:48 -0300

    Implementacion de React Router (beta), separando algunos apartados en diferentes paginas

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,22 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { RouterProvider, createHashRouter } from 'react-router-dom'
+import NotFoundPage from './NotFoundPage.tsx'
+import Categorias from './Categorias.tsx'
+import Carreras from './Carreras.tsx'
+
+// Creador de rutas
+const router = createHashRouter([
+  { path: '/', element: <App /> }, // asigno la ruta '/' al componente "app"
+  { path: '/categorias', element: <Categorias /> },
+  { path: '/carreras', element: <Carreras /> },
+  { path: '*', element: <NotFoundPage /> },
+])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    {/* la ruta es la constante declarada mas arriba */}
+    <RouterProvider router={router} />
   </StrictMode>,
 )
