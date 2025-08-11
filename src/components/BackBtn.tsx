e4ad79f48ea165afcfb35a2343b32417a35166ba
Tomas <liberfran.04@gmail.com>
2025-08-11 17:40:35 -0300

    Corrección en la intro: * Se corrigió un frame en el que la intro no salía cuando se iniciaba por primera vez la app.

--- src/components/BackBtn.tsx
+++
@@ -1,8 +0,0 @@
-function BackBtn() {
-    const prevPage = () =>{
-        history.back()
-    }
-    return <button onClick={prevPage} className="p-4 bg-blue-500 text-lg font-medium rounded-lg hover:bg-blue-600 cursor-pointer transition-background duration-500">Volver</button>;
-}
-
-export default BackBtn;
--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,7 +1,8 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import logo from '../assets/logo-fma-blanco.png'
 
 function Intro() {
+    const containerRef = useRef<HTMLDivElement>(null);
     const [logoClass, setLogoClass] = useState({
         opacity: 0,
         scale: 1,
@@ -9,13 +10,13 @@ function Intro() {
     const [loaderClass, setLoaderClass] = useState({
         opacity: 0,
     });
-    const [containerBackground, setContainerBackground] = useState('hidden'); // inicialmente la pantalla estara oculta, aunque cambia casi al instante
+    
     const [intro, setIntro] = useState(true);
 
     useEffect(() => {
         // si la intro no fue vista anteriormente, la muestro
         if (sessionStorage.getItem('intro') !== '1') {
-            setContainerBackground('bg-black');
+            // setContainerBackground('bg-black');
             document.body.classList.add('overflow-hidden');
             setTimeout(() => {
                 setLogoClass({
@@ -38,22 +39,25 @@ function Intro() {
                             scale: 25,
                         });
 
-                        setContainerBackground('bg-transparent');
+                        if (!containerRef.current) return;
 
+                        containerRef.current.classList.replace('bg-black', 'bg-transparent');
                         setTimeout(() => { // ultimo efecto a los 9.3s (final)
                             sessionStorage.setItem('intro', '1') // una vez vi la intro, lo seteo en 1 para que no vuelva a aparecer durante la sesion
                             setIntro(false);
-                            setContainerBackground('hidden');
+                            containerRef.current?.classList.toggle('hidden')
                             document.body.classList.remove('overflow-hidden');
                         }, 2300);
                     }, 1000);
                 }, 2000)
             }, 3000);
+        } else {
+            containerRef.current?.classList.replace('bg-black', 'hidden');
         }
     }, [])
 
     return <>
-        {intro ? <div className={'fixed z-5 h-screen w-screen flex flex-col gap-8 items-center justify-center text-gray-200 transition-background duration-2300 ease ' + containerBackground}>
+        {intro ? <div ref={containerRef} className={'fixed z-5 h-screen w-screen flex flex-col gap-8 items-center justify-center text-gray-200 transition-background duration-2300 ease bg-black'}>
             <img
                 src={logo}
                 alt="Logo FMA"
