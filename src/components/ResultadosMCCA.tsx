571c8cfbf79c9e7b75e3327a813886d871a12aef
Tomas <liberfran.04@gmail.com>
2025-08-14 14:01:00 -0300

    Prueba de tabla en mobile

--- src/components/ResultadosMCCA.tsx
+++ src/components/ResultadosMCCA.tsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react";
 import NavBar from "./Navbar";
 import Footer from "./Footer";
+
 type Pilotos = {
     gamertag: string,
     auto: string,
+    imagen: string,
     // fecha1: string,
     // fecha2: string,
     // fecha3: string,
@@ -29,9 +31,11 @@ function ResultadosMCCA() {
                     gamertag: pilotos.Gamertag,
                     auto: pilotos.Auto,
                     puntosTotales: pilotos.Totales,
+                    imagen: pilotos.Imagen,
                 }))
 
                 setPilotosLMH(pilotosLMH);
+                console.log(pilotosLMH)
             });
         fetch("https://opensheet.elk.sh/1Xy1nF3jUiaJYgKDCTFqDejCyxRMM7KhwfA3IRzSgAGY/Championship+Standings+GT3")
             .then(res => res.json())
@@ -42,6 +46,7 @@ function ResultadosMCCA() {
                     gamertag: pilotos.Gamertag,
                     auto: pilotos.Auto,
                     puntosTotales: pilotos.Totales,
+                    imagen: pilotos.Imagen
                 }))
 
                 setPilotosGT3(pilotosGT3);
@@ -52,7 +57,7 @@ function ResultadosMCCA() {
         <NavBar />
         <div className="flex flex-col my-35 text-sm lg:text-lg md:m-10 items-center gap-5 justify-center mx-2 text-gray-300">
             <h2 className="text-4xl font-medium">Resultados de LMH</h2>
-            <table className="table-auto border-collapse border border-gray-400 overflow-scroll xl:min-w-4xl">
+            {/* <table className="table-auto border-collapse border border-gray-400 overflow-scroll xl:min-w-4xl">
                 <thead>
                     <tr>
                         <th className="border border-gray-400 px-4 py-2">Gamertag</th>
@@ -83,13 +88,58 @@ function ResultadosMCCA() {
                     {pilotosGT3.map((piloto, index) => (
                         <tr key={index}>
                             <td className="border border-gray-400 px-4 py-2">{piloto.gamertag}</td>
-                            <td className="border border-gray-400 px-4 py-2">{piloto.auto}</td>
+                            <td className="border flex items-center gap-3 border-gray-400 px-4 py-2">{piloto.auto}</td>
                             <td className="border border-gray-400 px-4 py-2 text-right">{piloto.puntosTotales}</td>
                         </tr>
                     ))}
                 </tbody>
+            </table> */}
+            <table className="table-auto overflow-scroll xl:max-w-4xl">
+                <tbody>
+                    {pilotosLMH.map((piloto, index) => (
+                        <tr key={index}>
+                            <td className="">
+                                <div className="flex items-center gap-2.5">
+                                    <img src={piloto.imagen} alt="" className="w-full" />
+                                </div>
+                            </td>
+                            {/* <td className="border border-gray-400 px-4 py-2">
+
+                            </td>
+                            
+                            <td className="border border-gray-400 px-4 py-2">
+                                <div className="flex items-center gap-2.5">
+                                    <img src={piloto.prueba} alt="" className="w-full" />
+                                </div>
+                            </td> */}
+                        </tr>
+                    ))}
+                </tbody>
             </table>
-        </div>
+            <h2 className="text-4xl font-medium mt-20">Resultados de GT3</h2>
+            <table className="table-auto overflow-scroll xl:max-w-4xl">
+                <tbody>
+                    {pilotosGT3.map((piloto, index) => (
+                        <tr key={index}>
+                            <td className="">
+                                <div className="flex items-center gap-2.5">
+                                    <img src={piloto.imagen} alt="" className="w-full" />
+                                </div>
+                            </td>
+                            {/* <td className="border border-gray-400 px-4 py-2">
+
+                            </td>
+                            
+                            <td className="border border-gray-400 px-4 py-2">
+                                <div className="flex items-center gap-2.5">
+                                    <img src={piloto.prueba} alt="" className="w-full" />
+                                </div>
+                            </td> */}
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div >
         <Footer />
     </>
 }
